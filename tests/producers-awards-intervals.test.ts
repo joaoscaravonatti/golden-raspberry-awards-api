2ce2ee5ecb2c1eefc219822b04a1df6ad0578e0a
Joao Scaravonatti <jvscaravonatti@gmail.com>
2024-06-14 10:15:09 -0300

    add tests to validate response shape

--- tests/producers-awards-intervals.test.ts
+++ tests/producers-awards-intervals.test.ts
@@ -10,13 +10,28 @@ test.describe('GET /producers/awards/intervals', () => {
 
   test.after(() => process.kill(process.pid, 'SIGTERM'))
 
-  test.it('Should fetch intervals correctly', async () => {
+  test.it('should fetch intervals correctly', async () => {
     const result = await fetch('http://localhost:3000/producers/awards/intervals')
     const data = await result.json() as any
 
-    assert.deepEqual(!!data.min, true)
+    assert.deepEqual(result.status, 200)
+    assert.deepEqual(!!data, true)
+  })
+
+  test.it('should return an object with correct shape', async () => {
+    const result = await fetch('http://localhost:3000/producers/awards/intervals')
+    const data = await result.json() as any
+
     assert.deepEqual(!!data.max, true)
-    assert.deepEqual(data.min.length > 0, true)
-    assert.deepEqual(data.max.length > 0, true)
+    assert.deepEqual(typeof data.max[0].producer === 'string', true)
+    assert.deepEqual(typeof data.max[0].interval === 'number', true)
+    assert.deepEqual(typeof data.max[0].previousWin === 'number', true)
+    assert.deepEqual(typeof data.max[0].followingWin === 'number', true)
+    
+    assert.deepEqual(!!data.min, true)
+    assert.deepEqual(typeof data.min[0].producer === 'string', true)
+    assert.deepEqual(typeof data.min[0].interval === 'number', true)
+    assert.deepEqual(typeof data.min[0].previousWin === 'number', true)
+    assert.deepEqual(typeof data.min[0].followingWin === 'number', true)
   })
 })
